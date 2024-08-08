706d1ff7c468111b5d4541d6c42428d26f1722d3
Itzel Ramon <itzelramonf@gmail.com>
2024-08-08 17:09:17 -0500

    start portfolio

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import {Button} from "@nextui-org/button";
+
+export default function Home() {
+   return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+         <h1 className="text-3xl font-bold underline">Hello world!</h1>
+         <Button>Click me</Button>
+      </main>
+   );
+}
